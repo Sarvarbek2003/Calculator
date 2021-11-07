b9abb1cea303c4117c2ef5aa5e47754558c43a03
Sarvarbek2003 <89004202+Sarvarbek2003@users.noreply.github.com>
2021-11-07 16:19:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,112 +1,133 @@
-let num = document.querySelectorAll('.num')
-let sign = document.querySelectorAll('.sign')
-let equal = document.querySelector('.equal')
-let input = document.querySelector('.input')
-let remove = document.querySelectorAll('.remove-element')
-let st = ""
-let st2 = ''
-let num1 = []
-let amal = ""
-let nuq = 0 
-let inputt = ''
-let oz = 0 
-let count = 0
-for (let i=0; i<num.length;i++){
-    num[i].addEventListener('click', (event) => {
-        if (oz>0) count+=2
-        if (st2 == '') st2 = event.target.innerText.padStart(2, '0')
-        else st2 = event.target.innerText
-        if(event.target.innerText == '.' && nuq==0){
-            if (st.split('')[st.length-1]!=".") nuq++; st += st2
-        }else if(event.target.innerText != '.'){
-            if (st[0]!="0"){
-                st += event.target.innerText
-                nuq=0
-            }else {
-                if (st[1]==".") st += event.target.innerText
-                else st = event.target.innerText 
+class Calc{
+    constructor(){
+        this.num = document.querySelectorAll('.num')
+        this.sign = document.querySelectorAll('.sign')
+        this.equal = document.querySelector('.equal')
+        this.input = document.querySelector('.input')
+        this.remove = document.querySelectorAll('.remove-element')
+        this.st = ""
+        this.st2 = ''
+        this.num1 = []
+        this.amal = ""
+        this.nuq = 0 
+        this.inputt = ''
+        this.oz = 0 
+        this.count = 0
+    }
+
+     Num(){
+    for (let i=0; i<this.num.length;i++){
+        this.num[i].addEventListener('click', (event) => {
+            if (this.oz>0) this.count+=2
+            if (this.st2 == '') this.st2 = event.target.innerText.padStart(2, '0')
+            else this.st2 = event.target.innerText
+            if(event.target.innerText == '.' && this.nuq==0){
+                if (this.st.split('')[st.length-1]!=".") this.nuq++; this.st += this.st2
+            }else if(event.target.innerText != '.'){
+                if (this.st[0]!="0"){
+                    this.st += event.target.innerText
+                    this.nuq=0
+                }else {
+                    if (this.st[1]==".") this.st += event.target.innerText
+                    else this.st = event.target.innerText 
+                }
+                
             }
+            this.input.value = this.st 
+        })
+        
+    } this.num1 = this.st
+}
+ amallar(){
+    for (let i=0; i<this.sign.length; i++){
+        this.sign[i].addEventListener('click', (event) => {
+            this.nuq=0
+            this.st2 = ''
+            if (this.count<1. && this.st[this.st.length-1] == '+' ){    
+                this.oz++
+                this.st = this.st.split('+').join(event.target.innerText)
+                this.input.value =  this.st
+                this.amal = event.target.innerText
+            }else if(this.count<1 && this.st[this.st.length-1] == '-'){
+                this.oz++
+                this.st = this.st.split('-').join(event.target.innerText)
+                this.input.value =  this.st
+                this.amal = event.target.innerText
+            }else if(this.count<1 && this.st[this.st.length-1] == '✕'){
+                this.oz++
+                this.st = this.st.split('✕').join(event.target.innerText)
+                this.input.value =  this.st
+                this.amal = event.target.innerText
+            }else if(this.count<1 && this.st[this.st.length-1] == "÷" ){
+                this.oz++
+                this.st = this.st.split('÷').join(event.target.innerText)
+                this.input.value =  this.st
+                this.amal = event.target.innerText
+            }else{
+                if (this.count<1){
+                    this.st += event.target.innerText
+                    this.input.value = this.st
+                    this.amal = event.target.innerText
+                    this.oz++
+                }
+            }   
             
-        }
-        input.value = st 
+        })
+    }
+}
+ del(){
+    for (let i = 0; i<this.remove.length; i++){
+        this.remove[i].addEventListener('click',(event) => {
+            this.nuq=0
+            this.st2 = ''
+            this.a = this.st.split('')
+            if(event.target.innerText == 'R') this.a.pop()
+            else if (event.target.innerText == 'C') this.a = []; this.count = 0; this.oz = 0
+            this.st = ''
+            for (let i of this.a) this.st+=i
+            this.input.value = this.st
+            
+        })
+    }
+}
+ bajar(){
+    this.equal.addEventListener('click', (event) => {
+        if(this.amal=='+') {this.num1 = this.st.split('+'); if(this.num1[1] != '' ) { this.st = (+this.num1[0] + +this.num1[1])+''; this.input.value = this.st; this.num1[0]=this.st; this.amal = ''}}
+        else if(this.amal=='-'){this.num1 = this.st.split('-'); if(this.num1[1] != '' ) { this.st = (+this.num1[0] - +this.num1[1])+''; this.input.value = this.st; this.num1[0]=this.st; this.amal = ''}}
+        else if(this.amal=='÷'){this.num1 = this.st.split('÷'); if(this.num1[1] != '' ) { this.st = (+this.num1[0] / +this.num1[1])+''; this.input.value = this.st; this.num1[0]=this.st; this.amal = ''}}
+        else if(this.amal=='✕'){this.num1 = this.st.split('✕'); if(this.num1[1] != '' ) { this.st = (+this.num1[0] * +this.num1[1])+''; this.input.value = this.st; this.num1[0]=this.st; this.amal = ''}}
+        this.count=0
+        this.oz = 0
     })
-    
-} num1 = st
-for (let i=0; i<sign.length; i++){
-    sign[i].addEventListener('click', (event) => {
-        nuq=0
-        st2 = ''
-        if (count<2 && st[st.length-1] == '+' ){    
-            oz++
-            st = st.split('+').join(event.target.innerText)
-            input.value =  st
-            amal = event.target.innerText
-        }else if(count<1 && st[st.length-1] == '-'){
-            oz++
-            st = st.split('-').join(event.target.innerText)
-            input.value =  st
-            amal = event.target.innerText
-        }else if(count<1 && st[st.length-1] == '✕'){
-            oz++
-            st = st.split('✕').join(event.target.innerText)
-            input.value =  st
-            amal = event.target.innerText
-        }else if(count<1 && st[st.length-1] == "÷" ){
-            oz++
-            st = st.split('÷').join(event.target.innerText)
-            input.value =  st
-            amal = event.target.innerText
-        }else{
-            if (count<1){
-                st += event.target.innerText
-                input.value = st
-                amal = event.target.innerText
-                oz++
+}
+ klavitura(){
+    this.input.addEventListener('keyup', (event) => {
+        if (event.keyCode == 107 || event.keyCode == 109 || event.keyCode == 106 || event.keyCode == 111) {
+            this.count++
+            if (this.count > 1){
+                fn(event.target.value)
+                this.count=0
             }
-        }   
-        
-    })
-}
-for (let i = 0; i<remove.length; i++){
-    remove[i].addEventListener('click',(event) => {
-        nuq=0
-        st2 = ''
-        let a = st.split('')
-        if(event.target.innerText == 'R') a.pop()
-        else if (event.target.innerText == 'C') a = []; count = 0; oz = 0
-        st = ''
-        for (let i of a) st+=i
-        input.value = st
-        
-    })
-}
-equal.addEventListener('click', (event) => {
-    if(amal=='+') {num1 = st.split('+'); if(num1[1] != '' ) { st = (+num1[0] + +num1[1])+''; input.value = st; num1[0]=st; amal = ''}}
-    else if(amal=='-'){num1 = st.split('-'); if(num1[1] != '' ) { st = (+num1[0] - +num1[1])+''; input.value = st; num1[0]=st; amal = ''}}
-    else if(amal=='÷'){num1 = st.split('÷'); if(num1[1] != '' ) { st = (+num1[0] / +num1[1])+''; input.value = st; num1[0]=st; amal = ''}}
-    else if(amal=='✕'){num1 = st.split('✕'); if(num1[1] != '' ) { st = (+num1[0] * +num1[1])+''; input.value = st; num1[0]=st; amal = ''}}
-    count=0
-})
-
-input.addEventListener('keyup', (event) => {
-    // console.log(event.keyCode)
-    if (event.keyCode == 107 || event.keyCode == 109 || event.keyCode == 106 || event.keyCode == 111) {
-        count++
-        if (count > 1){
-            console.log(event.target.value)
-            fn(event.target.value)
-            count=0
         }
-    }
-    if (event.keyCode == 13){
-        fn(event.target.value)
-    }
-})
-
-function fn(arg){
-    st=arg
-    if(st.includes('+')) {num1 = st.split('+'); input.value = (+num1[0] + +num1[1]); st = (+num1[0] + +num1[1])+''; num1[0]=st }
-    else if(st.includes('-')){num1 = st.split('-'); input.value = (+(num1[0]) - +(num1[1])); st = (+num1[0] - +num1[1])+''; num1[0]=st }
-    else if(st.includes('/')){num1 = st.split('/'); input.value = (+(num1[0]) / +(num1[1])); st = (+num1[0] / +num1[1])+''; num1[0]=st}
-    else if(st.includes('*')){num1 = st.split('*'); input.value = (+(num1[0]) * +(num1[1])); st = (+num1[0] * +num1[1])+''; num1[0]=st } 
+        if (event.keyCode == 13){
+            fn(event.target.value)
+        }
+    })
 }
+ fn(arg){
+    this.st=arg
+    if(this.st.includes('+')) {this.num1 = this.st.split('+'); this.input.value = (+this.num1[0] + +this.num1[1]); this.st = (+this.num1[0] + +this.num1[1])+''; this.num1[0]=this.st }
+    else if(this.st.includes('-')){this.num1 = this.st.split('-'); this.input.value = (+(this.num1[0]) - +(this.num1[1])); this.st = (+this.num1[0] - +this.num1[1])+''; this.num1[0]=this.st }
+    else if(this.st.includes('/')){this.num1 = this.st.split('/'); this.input.value = (+(this.num1[0]) / +(this.num1[1])); this.st = (+this.num1[0] / +this.num1[1])+''; this.num1[0]=this.st}
+    else if(this.st.includes('*')){this.num1 = this.st.split('*'); this.input.value = (+(this.num1[0]) * +(this.num1[1])); this.st = (+this.num1[0] * +this.num1[1])+''; this.num1[0]=this.st } 
+}
+}
+let calk = new Calc()
+
+calk.Num()
+calk.amallar()
+calk.del()
+calk.bajar()
+calk.klavitura()
+
+
